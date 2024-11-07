5e926fd6c922f0b5d7a7681fa7014012ec356e0d
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-07 23:55:22 +0200

    add inventory dto

---
+++ src/main/java/com/gold_mining_app_backend/dto/InventoryDTO.java
@@ -0,0 +1,24 @@
+package com.gold_mining_app_backend.dto;
+
+import com.gold_mining_app_backend.modal.Inventory;
+import com.gold_mining_app_backend.util.LocalDateTimeConverter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InventoryDTO {
+    private String id;
+    private double qtyInStrock;
+    private String timeStamp;
+    private ProductDTO product;
+
+    public InventoryDTO(Inventory inventory) {
+        this.id = inventory.getId().toString();
+        this.qtyInStrock = inventory.getQtyInStrock();
+        this.timeStamp = LocalDateTimeConverter.convertLocalDateTime(inventory.getTimeStamp(), "dd, MMM-yyy HH:mm a");
+        this.product = new ProductDTO(inventory.getProduct());
+    }
+}
