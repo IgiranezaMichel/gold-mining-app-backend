b39cb7f2919322ae016a02fb9000e0a75d1c1208
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-07 23:56:23 +0200

    add query

--- src/main/java/com/gold_mining_app_backend/repository/InventoryRepository.java
+++ src/main/java/com/gold_mining_app_backend/repository/InventoryRepository.java
@@ -2,11 +2,15 @@ package com.gold_mining_app_backend.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import com.gold_mining_app_backend.enums.ProductQuality;
 import com.gold_mining_app_backend.modal.Inventory;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory,UUID>{
+
+    Page<Inventory> findAllByProductQuality(PageRequest of, ProductQuality quality);
 
 }
