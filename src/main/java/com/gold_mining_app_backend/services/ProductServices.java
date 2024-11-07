97eaeca872f9a315c0a058cd2c57cbdc335b7941
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-07 23:56:55 +0200

    add product services

--- src/main/java/com/gold_mining_app_backend/services/ProductServices.java
+++ src/main/java/com/gold_mining_app_backend/services/ProductServices.java
@@ -1,11 +1,48 @@
 package com.gold_mining_app_backend.services;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.gold_mining_app_backend.dto.ProductDTO;
+import com.gold_mining_app_backend.input.PageInput;
+import com.gold_mining_app_backend.modal.Product;
 import com.gold_mining_app_backend.repository.ProductRepository;
 
 @Service
 public class ProductServices {
-@Autowired private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    public ResponseEntity<String> createPrduct(Product product) {
+        try {
+            productRepository.save(product);
+            return new ResponseEntity<>("Product savec successful", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<String> deleteProduct(String id) {
+        try {
+            productRepository.findById(UUID.fromString(id))
+                    .orElseThrow(() -> new RuntimeException("Product not found"));
+            return new ResponseEntity<>("Product deleted successful", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    public List<ProductDTO> getAllProduct(PageInput pageInput) {
+        return productRepository.findAll(
+                PageRequest.of(pageInput.getPageNumber(), pageInput.getPageSize(), Sort.by(pageInput.getSortBy())))
+                .getContent().stream().map(ProductDTO::new).toList();
+    }
 }
