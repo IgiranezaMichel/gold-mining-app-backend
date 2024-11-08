546ec8b67ae9ceff48bec7ce1fcdcd92a5a12d01
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:25:18 +0200

    Sales consttructor

--- src/main/java/com/gold_mining_app_backend/modal/Sales.java
+++ src/main/java/com/gold_mining_app_backend/modal/Sales.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
 import com.gold_mining_app_backend.enums.SalesMethod;
+import com.gold_mining_app_backend.input.SalesInput;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -18,19 +19,27 @@ import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Sales {
-@UuidGenerator(style = Style.AUTO)
-@Id
-private UUID id;
-private LocalDateTime timeStamp;
-@ManyToOne(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY,targetEntity = Inventory.class)
-private Inventory inventory;
-@ManyToOne(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY,targetEntity = Orders.class,optional =true)
-private Orders order;
-@Enumerated(EnumType.STRING)
-private SalesMethod method;
+    @UuidGenerator(style = Style.AUTO)
+    @Id
+    private UUID id;
+    private LocalDateTime timeStamp;
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, targetEntity = Inventory.class)
+    private Inventory inventory;
+    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, targetEntity = Orders.class, optional = true)
+    private Orders order;
+    @Enumerated(EnumType.STRING)
+    private SalesMethod method;
+
+    public Sales(SalesInput salesInput, Inventory inventory, Orders order) {
+        this.inventory = inventory;
+        this.order = order;
+        this.method = salesInput.getMethod();
+        this.timeStamp = LocalDateTime.now();
+    }
 }
