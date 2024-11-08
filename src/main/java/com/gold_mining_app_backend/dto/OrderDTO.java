4639865d0dee63d24cdad559a44927c12def034e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 08:01:10 +0200

    add order dto

---
+++ src/main/java/com/gold_mining_app_backend/dto/OrderDTO.java
@@ -0,0 +1,30 @@
+package com.gold_mining_app_backend.dto;
+
+import java.time.LocalDateTime;
+import com.gold_mining_app_backend.enums.OrderStatus;
+import com.gold_mining_app_backend.modal.Orders;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDTO {
+private String id;
+private OrderStatus status;
+private double quantity;
+private LocalDateTime deadline;
+private ProductDTO product;
+private UserDTO user;
+private LocalDateTime timeStamp;
+public OrderDTO(Orders order) {
+    this.id = order.getId().toString();
+    this.status = order.getStatus();
+    this.quantity = order.getQuantity();
+    this.deadline = order.getDeadline();
+    this.product = new ProductDTO(order.getProduct());
+    this.user = new UserDTO(order.getUser());
+    this.timeStamp = order.getTimeStamp();
+}
+}
