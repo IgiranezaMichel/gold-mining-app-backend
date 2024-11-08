f872667da1f2bf8afa8edbfa98e123f8766cc8aa
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 08:00:54 +0200

    add service

--- src/main/java/com/gold_mining_app_backend/controller/SaleController.java
+++ src/main/java/com/gold_mining_app_backend/controller/SaleController.java
@@ -3,8 +3,22 @@ package com.gold_mining_app_backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gold_mining_app_backend.input.SalesInput;
+import com.gold_mining_app_backend.services.SaleServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/api/sales")
 public class SaleController {
+    @Autowired private SaleServices saleServices;
+@PostMapping("client/add-or-update")
+public ResponseEntity<String> createClientSale(@RequestBody SalesInput entity) {
+  return saleServices.createClientSale(entity);
+}
 
 }
