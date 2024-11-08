ba393c9cbaf2271ffccfe691086791d74f1f6015
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:24:25 +0200

    InventoryInput

---
+++ src/main/java/com/gold_mining_app_backend/input/InventoryInput.java
@@ -0,0 +1,15 @@
+package com.gold_mining_app_backend.input;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InventoryInput {
+private String id;
+private double qtyInStrock;
+private LocalDateTime timeStamp;
+private String productId;
+}
