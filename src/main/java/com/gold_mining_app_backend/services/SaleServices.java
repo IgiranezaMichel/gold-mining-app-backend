7ba9d7723ffd0629bac5c55631ce3d5b56924acd
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 08:03:12 +0200

    overload the method

--- src/main/java/com/gold_mining_app_backend/services/SaleServices.java
+++ src/main/java/com/gold_mining_app_backend/services/SaleServices.java
@@ -1,11 +1,18 @@
 package com.gold_mining_app_backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.gold_mining_app_backend.input.SalesInput;
 import com.gold_mining_app_backend.repository.SaleRepository;
 
 @Service
 public class SaleServices {
 @Autowired private SaleRepository salesRepository;
+
+public ResponseEntity<String> createClientSale(SalesInput entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createClientSale'");
+}
 }
--- src/main/java/com/gold_mining_app_backend/util/LocalDateTimeConverter.java
+++ src/main/java/com/gold_mining_app_backend/util/LocalDateTimeConverter.java
@@ -1,11 +1,19 @@
 package com.gold_mining_app_backend.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeConverter {
-public static String convertLocalDateTime(LocalDateTime ldt,String pattern){
-    DateTimeFormatter dtf=DateTimeFormatter.ofPattern(pattern);
-    return dtf.format(ldt);
-}
+    public static String convertLocalDateTime(LocalDateTime ldt, String pattern) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
+        return dtf.format(ldt);
+    }
+
+    public static String convertLocalDateTime(LocalDate ldt, String pattern) {
+        if (ldt == null)
+            return null;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
+        return dtf.format(ldt);
+    }
 }
