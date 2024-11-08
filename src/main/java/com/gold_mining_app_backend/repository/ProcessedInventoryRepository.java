6f8507bb6819dc58a29abceab370d52711ae238d
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:26:13 +0200

    add findTopByOrderByIdDesc

--- src/main/java/com/gold_mining_app_backend/repository/ProcessedInventoryRepository.java
+++ src/main/java/com/gold_mining_app_backend/repository/ProcessedInventoryRepository.java
@@ -1,5 +1,6 @@
 package com.gold_mining_app_backend.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 import com.gold_mining_app_backend.modal.ProcessedInventory;
 @Repository
 public interface ProcessedInventoryRepository extends JpaRepository<ProcessedInventory,UUID> {
+    Optional<ProcessedInventory> findTopByOrderByIdDesc();
 
 }
