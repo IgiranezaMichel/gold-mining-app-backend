f24b13ee5533f7eaa53c1d236e17a6c08d0f8080
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:32:16 +0200

    InventoryRepository

--- src/main/java/com/gold_mining_app_backend/services/SaleServices.java
+++ src/main/java/com/gold_mining_app_backend/services/SaleServices.java
@@ -1,18 +1,37 @@
 package com.gold_mining_app_backend.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.gold_mining_app_backend.input.SalesInput;
+import com.gold_mining_app_backend.modal.Inventory;
+import com.gold_mining_app_backend.modal.Orders;
+import com.gold_mining_app_backend.modal.Sales;
+import com.gold_mining_app_backend.repository.InventoryRepository;
+import com.gold_mining_app_backend.repository.OrderRepository;
 import com.gold_mining_app_backend.repository.SaleRepository;
 
 @Service
 public class SaleServices {
-@Autowired private SaleRepository salesRepository;
+    @Autowired
+    private SaleRepository salesRepository;
+    @Autowired
+    private InventoryRepository inventoryRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
-public ResponseEntity<String> createClientSale(SalesInput entity) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'createClientSale'");
-}
+    public ResponseEntity<String> createClientSale(SalesInput entity) {
+        try {
+            Inventory inventory = inventoryRepository.findById(UUID.fromString(entity.getId()))
+                    .orElseThrow(() -> new RuntimeException("Inventory not found"));
+            Orders orders = orderRepository.findById(UUID.fromString(entity.getOrderId())).orElse(null);
+            salesRepository.save(new Sales(entity, inventory, orders));
+            return ResponseEntity.ok("Sale created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
