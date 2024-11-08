c6ee589ccc452a4bfc1ab10e0a7fd6efe7182adb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 06:52:59 +0200

    remove unwanted spaces

--- src/main/java/com/gold_mining_app_backend/repository/ProductRepository.java
+++ src/main/java/com/gold_mining_app_backend/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.gold_mining_app_backend.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gold_mining_app_backend.enums.ProductCategory;
+import com.gold_mining_app_backend.enums.ProductQuality;
 import com.gold_mining_app_backend.modal.Product;
 
 public interface ProductRepository extends JpaRepository<Product,UUID> {
-
+    Optional<Product> findByQualityAndCategory(ProductQuality quality, ProductCategory category);
 }
