0b4bcfc6c8341c57b7bd1e6d3e9334b90b678f3f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:25:52 +0200

    add inventory repository

--- src/main/java/com/gold_mining_app_backend/repository/InventoryRepository.java
+++ src/main/java/com/gold_mining_app_backend/repository/InventoryRepository.java
@@ -1,16 +1,21 @@
 package com.gold_mining_app_backend.repository;
 
-import java.util.UUID;
-
+import java.util.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.gold_mining_app_backend.enums.ProductCategory;
 import com.gold_mining_app_backend.enums.ProductQuality;
 import com.gold_mining_app_backend.modal.Inventory;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory,UUID>{
 
     Page<Inventory> findAllByProductQuality(PageRequest of, ProductQuality quality);
+
+    Optional<Inventory> findTopByOrderByTimeStampDesc();
+
+    Optional<Inventory> findFirstByProductQualityAndProductCategoryOrderByTimeStampDesc(ProductQuality quality,
+            ProductCategory category);
 
 }
