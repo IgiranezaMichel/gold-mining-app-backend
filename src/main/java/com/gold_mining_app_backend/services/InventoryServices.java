3a8cfe9c261c6c707878b185b1bc51bd8fb99af6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-11-08 13:26:27 +0200

    manageInventory

--- src/main/java/com/gold_mining_app_backend/services/InventoryServices.java
+++ src/main/java/com/gold_mining_app_backend/services/InventoryServices.java
@@ -12,14 +12,18 @@ import org.springframework.stereotype.Service;
 import com.gold_mining_app_backend.dto.InventoryDTO;
 import com.gold_mining_app_backend.dto.PageDTO;
 import com.gold_mining_app_backend.enums.ProductQuality;
+import com.gold_mining_app_backend.input.InventoryInput;
 import com.gold_mining_app_backend.input.PageInput;
 import com.gold_mining_app_backend.modal.Inventory;
+import com.gold_mining_app_backend.modal.Product;
 import com.gold_mining_app_backend.repository.InventoryRepository;
+import com.gold_mining_app_backend.repository.ProductRepository;
 
 @Service
 public class InventoryServices {
     @Autowired
     private InventoryRepository inventoryRepository;
+    private ProductRepository productRepository;
 
     public ResponseEntity<String> deleteInventory(String id) {
         try {
@@ -30,6 +34,21 @@ public class InventoryServices {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    public Inventory manageInventory(String action, InventoryInput input, double qty) {
+        Product product = productRepository.findById(UUID.fromString(input.getProductId()))
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        Inventory inventory = inventoryRepository
+                .findFirstByProductQualityAndProductCategoryOrderByTimeStampDesc(product.getQuality(), product.getCategory())
+                .stream().findFirst().orElseThrow();
+        if (action.equals("add")) {
+            inventory.setQtyInStrock(inventory.getQtyInStrock() + qty);
+        } else if (action.equals("remove")) {
+            inventory.setQtyInStrock(inventory.getQtyInStrock() - qty);
+        }
+        inventory.setProduct(product);
+        return inventory;
     }
 
     public ResponseEntity<String> createInventory(Inventory inventoryInput) {
